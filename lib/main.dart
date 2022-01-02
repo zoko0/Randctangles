28deb94e55f8914c37a5e66e4e3c7f9e400326cb
bartosz.ciulkin <bartosz.ciulkin@bshg.com>
2022-01-02 19:00:49 +0100

    adjust painter size to screen size

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Space();
+    return const MaterialApp(home: Space());
   }
 }
 
@@ -18,12 +18,26 @@ class Space extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    var screenSize = MediaQuery.of(context).size;
+    var width = screenSize.width;
+    var height = screenSize.height;
+    double painterHeight = screenSize.height;
+    double painterWidth = screenSize.width;
+
+    if (width > height) {
+      painterWidth = height;
+    } else {
+      painterHeight = width;
+    }
+
+    return Center(
       child: Container(
-          color: Colors.black12,
-          child: CustomPaint(
-            painter: RectanglePainter(),
-          )
+        color: Colors.black12,
+        child: CustomPaint(
+          painter: RectanglePainter(),
+        ),
+        width: painterWidth - 100,
+        height: painterHeight - 100,
       ),
     );
   }
@@ -33,13 +47,14 @@ class RectanglePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
-        ..color = Colors.lime
-        ..strokeWidth = 1
-        ..style = PaintingStyle.fill;
+      ..color = Colors.lime
+      ..strokeWidth = 1
+      ..style = PaintingStyle.fill;
 
+    // final rect2 = Rect.fr
     final rect = Rect.fromPoints(
-      Offset(size.width * 1/4, size.height *1/4),
-      Offset(size.width * 3/4, size.height *3/4));
+        Offset(size.width * 1 / 4, size.height * 1 / 4),
+        Offset(size.width * 3 / 4, size.height * 3 / 4));
     canvas.drawRect(rect, paint);
   }
 
